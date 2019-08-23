4abe556709bed624b44a1879bc31d19b0a234d51
qwe <852962395@qq.com>
2019-08-23 16:33:47 +0800

    第一天内容

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+import '@/styles/index.less'
+import router from '@/routers/router.js'
+
+Vue.use(ElementUI)
+router.beforeEach((to, from, next) => {
+  let mytoken = localStorage.getItem('itcast_manager_35_token')
+  if (mytoken || to.path === '/login') {
+    next()
+  } else {
+    next({ path: '/login' })
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="home">
+    <p>首页</p>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
